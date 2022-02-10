fdd47508d3c4cd6ea7c4065eda8ad5f227a0ba7a
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-02-10 19:29:01 +0200

    added folder component and home component and install router

--- src/App.js
+++ src/App.js
@@ -7,27 +7,33 @@ import {
 } from "react-router-dom";
 import './App.css';
 import logo from './img/logo.png';
+import Home from './components/Home.js';
 function App() {
   return (
     <div className="App">
-      <header>
-        <div className="logo">
-          <img src={logo} alt="Logo" name="logo" />
-        </div>
-        <div className="links">
-          <ul>
-            <li>Home</li>
-            <li>Typing Test</li>
-            <li>Contact Us</li>
-            <li>About</li>
-          </ul>
-        </div>
-        <div className="contactUs">
-          <button className="btn-contact">Contact Us</button>
-        </div>
-      </header>
+      <Router>
+        <header>
+          <div className="logo">
+            <img src={logo} alt="Logo" name="logo" />
+          </div>
+          <div className="links">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li>Typing Test</li>
+              <li>Contact Us</li>
+              <li>About</li>
+            </ul>
+          </div>
+          <div className="contactUs">
+            <button className="btn-contact">Contact Us</button>
+          </div>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div >
+
   );
 }
-
 export default App;
