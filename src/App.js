549eac1b183b1e1bd45736a593d8d0d9031ff2a9
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-02-10 19:55:11 +0200

    update router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import {
 import './App.css';
 import logo from './img/logo.png';
 import Home from './components/Home.js';
+import Typing from './components/TypingTest';
+import About from './components/About';
 function App() {
   return (
     <div className="App">
@@ -18,10 +20,10 @@ function App() {
           </div>
           <div className="links">
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li>Typing Test</li>
+              <li><Link to="/" className="link">Home</Link></li>
+              <li><Link to="/typing" className="link">Typing Test</Link></li>
               <li>Contact Us</li>
-              <li>About</li>
+              <li><Link to="/about" className="link">About</Link></li>
             </ul>
           </div>
           <div className="contactUs">
@@ -29,7 +31,10 @@ function App() {
           </div>
         </header>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route exact="true" path="/" element={<Home />}></Route>
+          <Route path="/typing" element={<Typing />}></Route>
+          <Route path="/about" element={<About />}></Route>
+
         </Routes>
       </Router>
     </div >
