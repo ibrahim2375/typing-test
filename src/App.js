5c636654218d746158359c37be0881d13bd8673f
IbraHam733 <98653954+ibrahim2375@users.noreply.github.com>
2022-02-10 19:15:34 +0200

    created css file and finished nav for big screen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
-
+import logo from './img/logo.png';
 function App() {
   return (
     <div className="App">
+      <header>
+        <div className="logo">
+          <img src={logo} alt="Logo" name="logo" />
+        </div>
+        <div className="links">
+          <ul>
+            <li>Home</li>
+            <li>Typing Test</li>
+            <li>Contact Us</li>
+            <li>About</li>
+          </ul>
+        </div>
+        <div className="contactUs">
+          <button className="btn-contact">Contact Us</button>
+        </div>
+      </header>
     </div >
   );
 }
